30dd0ba10356c743618049208a9b820433a7e8af
Malte S. Stretz <mss@apache.org>
2011-07-20 18:42:04 +0200

    Add allowed_hosts restriction to DomainDispatcher.

--- eg/domain.psgi
+++ eg/domain.psgi
@@ -3,5 +3,6 @@ use warnings;
 use lib qw(lib);
 use Plack::App::Nginx::Auth::DomainDispatcher;
 Plack::App::Nginx::Auth::DomainDispatcher->new(
-    default_domain => 'example.com'
+    default_domain => 'example.com',
+    allowed_hosts  => '192.0.43.10',
 );
--- lib/Plack/App/Nginx/Auth/DomainDispatcher.pm
+++ lib/Plack/App/Nginx/Auth/DomainDispatcher.pm
@@ -1,26 +1,48 @@
 package Plack::App::Nginx::Auth::DomainDispatcher;
 use parent qw(Plack::App::Nginx::Auth);
-use Plack::Util::Accessor qw(default_domain);
+use Plack::Util::Accessor qw(default_host allowed_hosts);
 
 use strict;
 use warnings;
 
+use Net::IP;
+
 use Nginx::Auth;
+
+sub prepare_app {
+    my($self) = @_;
+
+    my $hosts = $self->allowed_hosts;
+    $hosts ||= [];
+    $hosts = [ $hosts ] unless ref $hosts;
+    map { $_ = Net::IP->new($_) or die Net::IP::Error() } @{$hosts};
+    $self->allowed_hosts($hosts);
+}
 
 sub call {
     my($self, $env) = @_;
 
     my $req = $self->new_request($env);
-    return 0 unless $req->auth_username;
+    return 403 unless $req->auth_username;
 
     my($user, $domain) = split(/[@+%]/, $req->auth_username, 2);
-    $domain ||= $self->default_domain;
-    return 0 unless $domain;
+    $domain ||= $self->default_host;
+    return 403 unless $domain;
 
-    my $host = Nginx::Auth::resolve_host($domain);
-    return 0 unless $host;
+    my $server = Nginx::Auth::resolve_host($domain);
+    return 403 unless $server;
+    my $ip = Net::IP->new($server);
+    foreach my $net (@{$self->allowed_hosts}) {
+        my $r = $net->overlaps($ip);
+        next unless defined $r;
+        if ($r != $Net::IP::IP_NO_OVERLAP) {
+            $ip = undef;
+            last;
+        }
+    }
+    return 403 if $ip;
 
-    return { 'server' => $host };
+    return { 'server' => $server };
 }
 
 1;
