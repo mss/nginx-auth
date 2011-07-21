70f856a0d0b49a44cadfa5b59b33271a10ae381a
Malte S. Stretz <mss@apache.org>
2011-07-21 12:02:00 +0200

    Fixup white- and blacklists.

--- eg/domain.psgi
+++ eg/domain.psgi
@@ -3,6 +3,6 @@ use warnings;
 use lib qw(lib);
 use Plack::App::Nginx::Auth::DomainDispatcher;
 Plack::App::Nginx::Auth::DomainDispatcher->new(
-    default_domain => 'example.com',
-    allowed_hosts  => '192.0.43.10',
+    default_host   => 'example.com',
+    allowed_hosts  => 'localhost',
 );
--- lib/Plack/App/Nginx/Auth/DomainDispatcher.pm
+++ lib/Plack/App/Nginx/Auth/DomainDispatcher.pm
@@ -1,6 +1,10 @@
 package Plack::App::Nginx::Auth::DomainDispatcher;
 use parent qw(Plack::App::Nginx::Auth);
-use Plack::Util::Accessor qw(default_host allowed_hosts);
+use Plack::Util::Accessor qw(
+    default_host
+    allowed_hosts
+    denied_hosts
+);
 
 use strict;
 use warnings;
@@ -9,14 +13,40 @@ use Net::IP;
 
 use Nginx::Auth;
 
-sub prepare_app {
-    my($self) = @_;
+sub new {
+    my $self = shift->SUPER::new(@_);
 
-    my $hosts = $self->allowed_hosts;
-    $hosts ||= [];
-    $hosts = [ $hosts ] unless ref $hosts;
-    map { $_ = Net::IP->new($_) or die Net::IP::Error() } @{$hosts};
-    $self->allowed_hosts($hosts);
+    my %defaults = (
+        allowed_hosts => [ ],
+        denied_hosts  => [ qw(224.0.0.0/4 ff00::/8) ],
+    );
+    $self->_map_hosts(sub {
+        my($hosts, $key) = @_;
+        $hosts ||= $defaults{$key};
+        $hosts = [ $hosts ] unless ref $hosts;
+        return $hosts unless $key eq 'allowed_hosts';
+        push(@{$hosts}, $self->default_host) if $self->default_host;
+        return $hosts;
+    });
+
+    return $self;
+}
+
+sub prepare_app {
+    my $self = shift;
+
+    $self->_map_hosts(sub {
+        my($hosts, $key) = @_;
+        $hosts = [] unless $hosts;
+        $hosts = [ $hosts ] unless ref $hosts;
+        return [ grep {$_} map {
+            unless (ref) {
+                $_ = Nginx::Auth::resolve_host($_) unless m(/);
+                $_ = Net::IP->new($_);
+            }
+            $_;
+        } @{$hosts} ];
+    });
 }
 
 sub call {
@@ -30,19 +60,39 @@ sub call {
     return 403 unless $domain;
 
     my $server = Nginx::Auth::resolve_host($domain);
-    return 403 unless $server;
-    my $ip = Net::IP->new($server);
+    return 403 unless $self->_check_hosts($server);
+
+    return { 'server' => $server };
+}
+
+sub _map_hosts {
+    my($self, $sub) = @_;
+
+    foreach my $key (qw(allowed_hosts denied_hosts)) {
+        $self->{$key} = $sub->($self->{$key}, $key);
+    }
+}
+
+sub _check_hosts {
+    my($self, $ip) = @_;
+
+    return unless $ip;
+    $ip = Net::IP->new($ip) unless ref $ip;
+
+    my $found;
     foreach my $net (@{$self->allowed_hosts}) {
         my $r = $net->overlaps($ip);
         next unless defined $r;
-        if ($r != $Net::IP::IP_NO_OVERLAP) {
-            $ip = undef;
-            last;
-        }
+        $found = $r != $Net::IP::IP_NO_OVERLAP;
+        last if $found;
     }
-    return 403 if $ip;
-
-    return { 'server' => $server };
+    return unless $found;
+    foreach my $net (@{$self->denied_hosts}) {
+        my $r = $net->overlaps($ip);
+        next unless defined $r;
+        return if $r != $Net::IP::IP_NO_OVERLAP;
+    }
+    return 1;
 }
 
 1;
